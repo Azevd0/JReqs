3d71b7a49db322667a03c3a2e419ec3ebcf93450
Azevd0 <davyson887@gmail.com>
2026-08-15 22:46:16 -0300

    adding jakarta and parsson lib

--- src/utils/JsonFormatter.java
+++ src/utils/JsonFormatter.java
@@ -1,17 +1,21 @@
 package utils;
 
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.json.Json;
+import jakarta.json.JsonReader;
+import jakarta.json.JsonValue;
+import jakarta.json.stream.JsonGenerator;
 
 public class JsonFormatter {
-    public static String formatJson(String jsonString){
-        if(jsonString == null || jsonString.isBlank()){
+    public static String formatJson(String jsonString) {
+        if (jsonString == null || jsonString.isBlank()) {
             return "";
         }
-//        try(StringReader jsonReader = new StringReader(jsonString)){
-//
-//        }catch (Exception ex){
-//
-//        }
+
+
         return jsonString;
     }
 }
